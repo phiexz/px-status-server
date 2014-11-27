90b181f803dd858158a46d75f6e49deae0171ab1
phiexz <dhiika@phiexz.com>
2014-11-27 11:40:46 +0700

    use bootstrap 3.3.1

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name=description content="ServerStatus">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
   </head>
   <body>
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
   </body>
 </html>
