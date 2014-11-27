53e5845a2eee03c50faf348cc057a34e7a3d045a
phiexz <dhiika@phiexz.com>
2014-11-27 11:34:22 +0700

    initial status

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PX ServerStatus</title>
+    <meta charset=utf-8>
+    <meta http-equiv=X-UA-Compatible content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name=description content="ServerStatus">
+  </head>
+  <body>
+  </body>
+</html>
